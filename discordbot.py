23ecbdb323c330c1fe3f5df0f97b64281f66e0cc
hachian <41578466+hachian@users.noreply.github.com>
2020-04-13 23:06:44 +0900

    [fix] channel id string to number

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    ch = client.get_channel('638744494610841603')
+    ch = client.get_channel(638744494610841603)
     await ch.send("Wah Gwaan!")
 
     
