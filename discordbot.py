ee2e00f133a8391b69f8970a9af17f8eabd7bf20
hachian <41578466+hachian@users.noreply.github.com>
2020-04-13 22:22:21 +0900

    [add] response on ready

--- discordbot.py
+++ discordbot.py
@@ -3,8 +3,14 @@ import os
 import traceback
 import random
 
+client = discord.Client()
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
 
 
 @bot.event
