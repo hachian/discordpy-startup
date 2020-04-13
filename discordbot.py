990c360bf53a1d27049e23055dd1e7d55c9427b7
hachian <41578466+hachian@users.noreply.github.com>
2020-04-13 22:40:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,10 @@
 import os
 import traceback
 import random
-import discord
 
-bot = discord.ext.commands.Bot(command_prefix='/')
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
